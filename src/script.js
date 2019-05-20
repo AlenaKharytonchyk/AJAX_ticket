bf3b5c722e2f2619862f20f2d97b39afb078d5c5
Alena <alena.kharytonchyk@outlook.com>
2019-05-20 09:54:44 +0300

    chore: remove function

--- src/script.js
+++ src/script.js
@@ -169,29 +169,6 @@ class Movie {
       });
     });
   }
-
-  attachmodalWindow() {
-    const modal = document.querySelector('#modal');
-
-
-    const modalOverlay = document.querySelector('#modal-overlay');
-
-
-    const closeButton = document.querySelector('#close-button');
-
-
-    const openButton = document.querySelector('#open-button');
-
-    closeButton.addEventListner('click', function () {
-      modal.classList.toggle('closed');
-      modalOverlay.classList.toggle('closed');
-    });
-
-    openButton.addEventListner('click', function () {
-      modal.classList.toggle('closed');
-      modalOverlay.classList.toggle('closed');
-    });
-  }
 }
 
 const config = {
